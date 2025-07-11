7b1499f8f9751e537c3d28be244e798ea3e5b267
Federico Maciel <federicomaciel77@gmail.com>
2025-07-10 21:00:19 -0300

    Instalamos Firebase, Firestore y dataenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import express from 'express';
 import cors from 'cors';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
